e241efa30ab17b644dd31376319835e7c9cc52f1
Rostyslav <rooostik2016@gmail.com>
2026-05-01 22:08:52 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -9,11 +9,7 @@ function convertToObject(sourceString) {
   const parsedRules = sourceString
     .trim()
     .split(';')
-    .map((e) =>
-      e
-        .split(':')
-        .map((v) => v.trim())
-        .filter((l) => l.length > 0));
+    .map((e) =>e.split(':').map((v) => v.trim()).filter((l) => l.length > 0));
 
   return parsedRules.reduce(
     (acc, cur) => Object.assign(acc, { [cur[0]]: cur[1] }),
