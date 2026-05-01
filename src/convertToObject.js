ef81b3f59662f4711d88ec878fdc3355c77604a3
Rostyslav <rooostik2016@gmail.com>
2026-05-01 21:39:55 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const map = new Map();
+
+  const trimmed = sourceString
+    .trim()
+    .split(';')
+    .flatMap((e) => e.split(':'))
+    .flatMap((e) => e.trim())
+    .filter((e) => e.length > 0);
+
+  for (let i = 0; i < trimmed.length; i += 2) {
+    map.set(trimmed[i], trimmed[i + 1]);
+  }
+
+  return Object.fromEntries(map);
 }
 
 module.exports = convertToObject;
